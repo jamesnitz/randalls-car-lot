805499bf66ab33c3ab70d34ab2440e1fac9d130f
jamesnitz <jamesanitz2@gmail.com>
2020-03-03 09:08:56 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace randalls_car_lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ayyy it's Randy");
+        }
+    }
+}
