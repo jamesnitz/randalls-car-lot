3cdf1271f589724eb983777d129960003171fe7a
jamesnitz <jamesanitz2@gmail.com>
2020-03-03 09:46:31 -0600

    second sale

--- Program.cs
+++ Program.cs
@@ -10,19 +10,33 @@ namespace randalls_car_lot
       Console.WriteLine("Ayyy it's Randy");
       List<Dictionary<string, Dictionary<string, string>>> vehicles = new List<Dictionary<string, Dictionary<string, string>>>();
 
-      Dictionary<string, Dictionary<string, string>> saleInfo = new Dictionary<string, Dictionary<string, string>>();
+      Dictionary<string, Dictionary<string, string>> bigmanSalesInfo = new Dictionary<string, Dictionary<string, string>>();
 
-      Dictionary<string, string> VehicleInfo = new Dictionary<string, string> { { "year", "1999" }, { "model", "cherokee" }, { "make", "jeep" }, { "color", "maroon" } };
+      Dictionary<string, string> bigmanVehicleInfo = new Dictionary<string, string> { { "year", "1999" }, { "model", "cherokee" }, { "make", "jeep" }, { "color", "maroon" } };
 
-      Dictionary<string, string> sales_agent = new Dictionary<string, string> { { "mobile", "555-555-4444" }, { "last_name", "Bigman" }, { "first_name", "Slick" } };
+      Dictionary<string, string> bigmanSalesAgent = new Dictionary<string, string> { { "mobile", "555-555-4444" }, { "last_name", "Bigman" }, { "first_name", "Slick" } };
 
-      Dictionary<string, string> credit = new Dictionary<string, string> { { "credit_provider", "The Creditor" }, { "account", "123456789" } };
+      Dictionary<string, string> bigmanCredit = new Dictionary<string, string> { { "credit_provider", "The Creditor" }, { "account", "123456789" } };
 
-      saleInfo.Add("vehicleInfo", VehicleInfo);
-      saleInfo.Add("sales_agent", sales_agent);
-      saleInfo.Add("credit", credit);
+      bigmanSalesInfo.Add("vehicleInfo", bigmanVehicleInfo);
+      bigmanSalesInfo.Add("sales_agent", bigmanSalesAgent);
+      bigmanSalesInfo.Add("credit", bigmanCredit);
 
-      vehicles.Add(saleInfo);
+      vehicles.Add(bigmanSalesInfo);
+
+      Dictionary<string, Dictionary<string, string>> davisSalesInfo = new Dictionary<string, Dictionary<string, string>>();
+
+      Dictionary<string, string> davisVehicleInfo = new Dictionary<string, string> { { "year", "2013" }, { "model", "Crosstrek" }, { "make", "Subaru" }, { "color", "White" } };
+
+      Dictionary<string, string> DavisSalesAgent = new Dictionary<string, string> { { "mobile", "555-555-4444" }, { "last_name", "Dad" }, { "first_name", "My" } };
+
+      Dictionary<string, string> DavisCredit = new Dictionary<string, string> { { "credit_provider", "The Creditor" }, { "account", "234234234234" } };
+
+      davisSalesInfo.Add("davisVehicleInfo", davisVehicleInfo);
+      davisSalesInfo.Add("sales_agent", DavisSalesAgent);
+      davisSalesInfo.Add("credit", DavisCredit);
+
+      vehicles.Add(davisSalesInfo);
 
 
     }
